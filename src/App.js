4aae4f998b01d8803286f02aa21cb56366f891f4
jamesburgess44 <jamesburgess44@yahoo.com>
2021-07-27 14:43:12 -0600

    we added in notes to explain the logic of our next three user stores that all hve to do with leaving comments on videos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+// Friday of last week we created a render loop in our app and it took us until Monday after our lunch break to actually understand what was happening.
+// We corrected the issue but lost a little more than a day of work tyring to fix problems that were actually just expired API Key's.
+// Today (tues/27th) we started the day with very little working but managed to get a default video to play, 
+// user can search and will be shown a list of 10 videos that they can then click on and have play on the player. 
+// Also, the user can click on a related video and it will start playing in the video player.
+
+// (7.5 points) As a user, I want to add a comment to a video in the comment section. 
+//   in the last our of our day today (tues/27th) we attempted to get our POST component working so that we could add a comment using a form into our database. 
+//   we were attempting to use similar methods that we had previously used in the music database. 
+//   in its current state attempting to add a video results in a "[27/Jul/2021 14:11:46] "POST /youtube_app/ HTTP/1.1" 400 81" Error that we did not have time to figure out.
+//   we looked to make sure that we are passing all required data with the POST request and the next thing we were going to check is to console.log(this.props.defautVideo)
+//   to see how it's passing the videoId in. 
+
+// (5 points) As a user, I want to like or dislike a comment in the comment section.
+//   the basic logic that we had planned on using for this was to have it start with a default value of 0 and when you click like or dislike it simply gives a +1 
+//   to the current value of like or dislike to keep a running total. this would require the info from the GET to know what the couter was currently at and then 
+//   ading 1 and using a PATCH request we had worked on in POSTMAN "http://127.0.0.1:8000/youtube_app/like/2"
+
+// (7.5 points) As a user, I want to reply to another comment in the comment section.
+//   the logic we were planning on using would use the videoId as a FK, the database also would store the origionalComment so they could be displayed in the correct order.
+//   we played aroud with the idea of having reply button in the origional comment or spending some time researching the best way to make this user friendly and look 
+//   like other comment sections work in youtbue or compareable applications. 
+
+
 import React, { Component } from 'react';
 import axios from 'axios';
 import NavBar from './Components/Navbar/navBar';
