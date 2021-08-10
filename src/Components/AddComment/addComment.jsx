847e403bdc531265508fa440d2b5ba58967a6bee
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-09 22:40:43 -0500

    Testing to see if fork was successful.

--- src/Components/AddComment/addComment.jsx
+++ src/Components/AddComment/addComment.jsx
@@ -4,31 +4,31 @@ import './addComment.css';
 
 // This comment section was originally from a Bootsnipp.com user named: Besnikhetemi
 // Check out his profile here: https://bootsnipp.com/Besnikhetemi
+// Or on here: http://www.facebook.com/besnik.hetemii
 // I'm adapting it to fit my current comment section that I want to show up on this web app.
 export default function AddComment() {
     return (
         <section class="comments">
             <article class="comment">
-                <a class="comment-img" href="#non">
-                    <img src="https://pbs.twimg.com/profile_images/444197466133385216/UA08zh-B.jpeg" alt="" width="50" height="50" />
+                <a class="comment-img" href="">
+                    <img src="" alt="" width="50" height="50" />
                 </a>
                 <div class="comment-body">
                     <div class="text">
                     <p>Hello, this is an example from me</p>
                     </div>
-                    <p class="attribution">by <a href="#non">Besnik Hetemi</a> at 14:23pm, 4 Dec 2015</p>
+                    <p class="attribution">by <a href="">User Two</a> at 14:23pm, 4 Dec 2015</p>
                 </div>
             </article>
             <article class="comment">
-                <a class="comment-img" href="#non">
-                    <img src="https://pbs.twimg.com/profile_images/444197466133385216/UA08zh-B.jpeg" alt="" width="50" height="50" />
+                <a class="comment-img" href="">
+                    <img src="" alt="" width="50" height="50" />
                 </a>
                 <div class="comment-body">
                     <div class="text">
-                        <p>if you are interested for  more about me visited my profile on social page</p>
-                        <p>To visit me you can click my name  <a target="_blank" href="http://www.facebook.com/besnik.hetemii">Besnik Hetemi</a> and send me frends request or send me a message in inbox</p>
+                        <p>Here is a comment</p>
                     </div>
-                    <p class="attribution">by <a href="#non">Besnik Hetemi</a> at 14:23pm, 4 Dec 2015</p>
+                    <p class="attribution">by <a href="">User One</a> at 14:23pm, 4 Dec 2015</p>
                 </div>
             </article>
         </section>
